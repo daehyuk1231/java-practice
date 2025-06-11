1322e844de22322d89b3e5285b24008c5461ea9f
daehyuk1231 <daehyuk1231@naver.com>
2025-06-11 23:31:32 +0900

    while

--- sec04/chap04/Ex01.java
+++ sec04/chap04/Ex01.java
@@ -17,17 +17,17 @@ public class Ex01 {
             System.out.println("같은 실수를 반복한다.");
         }
 
-        double popInBillion = 7.837;
+//        double popInBillion = 7.837;
 
         //  ⭐️ break 를 통한 반복 탈출
-        while (true) {
-            System.out.println("세계인구: " + (popInBillion -= 0.1));
-            if (popInBillion <= 0) break;
+//        while (true) {
+//            System.out.println("세계인구: " + (popInBillion -= 0.1));
+//            if (popInBillion <= 0) break;
+//
+//            System.out.println("인간의 욕심은 끝이 없고");
+//            System.out.println("같은 실수를 반복한다.");
+//        }
 
-            System.out.println("인간의 욕심은 끝이 없고");
-            System.out.println("같은 실수를 반복한다.");
-        }
-
-        System.out.println("인류 멸종");
+//        System.out.println("인류 멸종");
     }
 }
